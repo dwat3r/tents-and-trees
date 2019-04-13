e67a3155c10b73ff2101d50dffe3d179671c7653
dwat3r <hechtloliver@gmail.com>
2019-04-13 22:10:34 +0200

    first solution attempt, needs verification

--- TentsAndTrees.hs
+++ TentsAndTrees.hs
@@ -1,6 +1,44 @@
+{-# LANGUAGE RecordWildCards #-}
 module TentsAndTrees where
 
+import qualified Data.Map as M
+import qualified Data.Vector as V
+import Debug.Trace
+
+data Field = Grass | Tent | Tree deriving (Eq, Show)
+
+data Puzzle = Puzzle {
+    fields :: (M.Map (Int,Int) Field),
+    horizontal :: (M.Map Int Int),
+    vertical :: (M.Map Int Int)
+  } deriving Show
+
+solve puzzle = 
+  --M.union (fields puzzle) $
+  reduceProblem puzzle $
+  removeOrphanTents puzzle $ 
+  filter (\t -> snd t `M.notMember` (M.filter (==0) $ vertical puzzle)) $
+  filter (\t -> fst t `M.notMember` (M.filter (==0) $ horizontal puzzle)) $
+  M.keys $
+  M.differenceWith (\_ _ -> Nothing)
+    (szigetFest ((M.size $ horizontal puzzle) - 1))
+    (fields puzzle)
+    where
+      szigetFest size = M.fromList $ zip ((,) <$> [0..size] <*> [0..size]) $ repeat Tent
+      removeOrphanTents p = filter (not . null . (neighTrees p))
+      reduceProblem _ [] = []
+      reduceProblem p@Puzzle{..} ((x,y):ts)
+          | length (neighTrees p (x,y)) > 0 &&
+            horizontal M.! x > 0 &&
+            vertical M.! y > 0 = (x,y):(reduceProblem reduced $ removeOrphanTents reduced ts)
+          | horizontal M.! x == 0 ||
+            vertical M.! y == 0 = reduceProblem p ts
+          | otherwise = error "?? problem in code"
+              where 
+                reduced = Puzzle 
+                    (M.delete (head $ neighTrees p (x,y)) fields)
+                    (M.adjust (subtract 1) x horizontal)
+                    (M.adjust (subtract 1) y vertical)
 
 
-data Field = Tent | Tree deriving (Eq, Show)
-
+neighTrees Puzzle{..} (x,y) = filter (`M.member` fields) [(x-1,y),(x,y-1), (x+1,y), (x,y+1)]
--- Tests.hs
+++ Tests.hs
@@ -4,180 +4,146 @@ import qualified Data.Map as M
 import qualified Data.Vector as V
 import TentsAndTrees
 
-data Puzzle = Puzzle {
-    fields :: (M.Map (Int,Int) Field),
-    horizontal :: (V.Vector Int),
-    vertical :: (V.Vector Int)
-  } deriving Show
+puzzle tree horizontal vertical = 
+  Puzzle 
+    (M.fromList $ zip tree $ repeat Tree)
+    (M.fromList $ zip [0..] horizontal)
+    (M.fromList $ zip [0..] vertical)
 
-example = Puzzle (M.fromList [
-  ((0,0),Tent),
-  ((0,1),Tree),
-  ((0,2),Tent),
-  ((1,0),Tree),
-  ((1,4),Tent),
-  ((2,1),Tent),
-  ((2,2),Tree),
-  ((2,4),Tree),
-  ((3,3),Tent),
-  ((3,4),Tree),
-  ((4,1),Tent),
-  ((4,2),Tree),
-  ((4,4),Tree),
-  ((5,4),Tent)
+first = puzzle [(1,0),(4,1),(1,2),(3,2),(4,3)] [2,0,1,0,2] [2,0,2,0,1]
+
+firstTents = [(0,0),(0,2),(2,2),(4,0),(4,4)]
+
+second = puzzle [(0,1),(1,0),(2,2),(2,4),(3,4),(4,2),(4,4)] [2,1,1,1,1,1] [1,2,1,1,2,0]
+
+secondTents = [(0,0),(0,2),(1,4),(2,1),(3,3),(4,1),(5,4)]
+
+easy = puzzle [(0,0),(2,2)] [0,2,0] [1,0,1]
+      
+toPuzzle chars = concatMap (\(y, cs) -> map (\(x, _) -> (x,y)) $ filter (\(x, c) -> c == 't') $ zip [0..] cs) $ zip [0..] chars
+
+
+easy8x8 = puzzle (toPuzzle 
+    [
+      " t      ",
+      "   t  t ",
+      "   t   t",
+      "        ",
+      "    t t ",
+      "t  t   t",
+      "      t ",
+      "t       "
+    ])
+    [1, 3, 0, 1, 3, 1, 2, 1]
+    [1, 2, 1, 2, 0, 3, 0, 3]
+      
+hard20x20 = puzzle (toPuzzle
+  [
+    " t   tt    tt   t   ",
+    "tt            t    t",
+    "     t  t         t ",
+    "   t    t tt       t",
+    " t    t       t  t  ",
+    "  t       t t t     ",
+    "          t   tt   t",
+    " t    t         t   ",
+    " t  t       t      t",
+    "    t  t  tt  t     ",
+    "             tt t   ",
+    "      tt          t ",
+    " t  t    t          ",
+    " t  t               ",
+    " t   t     t  t t   ",
+    " t     t t          ",
+    "         t  tt   t t",
+    " tt    t            ",
+    "     tt   t t t t  t",
+      "  t     t      t t  "
   ])
-  (V.fromList [2,1,1,1,1,1])
-  (V.fromList [1,2,1,1,2,0])
+  [6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8]
+  [4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6]
+
+-- "TentsAndTrees C - 13x13"
+tntC13x13 = puzzle (toPuzzle
+  [
+    "t          t ",
+    "t t t    tt  ",
+    "      t     t",
+    "         t t ",
+    "    t   t  t ",
+    " t t      t  ",
+    "        t  t ",
+    " t  t t     t",
+    "t            ",
+    "    t  tt    ",
+    "     t       ",
+    "    t  t t  t",
+    " t   t    t  "
+  ])
+  [2, 4, 2, 4, 2, 2, 4, 2, 2, 3, 2, 2, 4]
+  [2, 4, 0, 4, 2, 3, 2, 2, 4, 2, 4, 1, 5]
+
+-- "TentsAndTrees C - 13x13 - 2"
+tntC13x13n2 = puzzle (toPuzzle
+  [
+    "             ",
+    "  t    t t  t",
+    "t     tt     ",
+    "   t t     t ",
+    "         t   ",
+    "t    t    t  ",
+    "  t       t  ",
+    "      t     t",
+    "t tt      t  ",
+    "      t    t ",
+    " t  t t  t   ",
+    "      t   tt ",
+    "  t t t      "
+  ])
+  [1, 5, 0, 5, 1, 3, 2, 3, 2, 3, 3, 2, 4]
+  [3, 2, 3, 3, 3, 2, 2, 3, 1, 3, 3, 2, 4]
+   
+-- "TentsAndTrees C - 13x13 - 3"
+tntC13x13n3 = puzzle (toPuzzle
+  [
+    " t  t        ",
+    "     t t    t",
+    "      t  t t ",
+    " t         t ",
+    " t  t  tt    ",
+    "    t     t t",
+    " t  tt       ",
+    "t     t     t",
+    "  tt  t  t   ",
+    "             ",
+    "        t   t",
+    "   t    t t  ",
+    " t   t       "
+  ])
+  [4, 2, 1, 5, 2, 4, 2, 4, 1, 3, 1, 2, 3]
+  [3, 2, 2, 4, 2, 2, 3, 3, 2, 4, 1, 1, 5]
+
+-- "TentsAndTrees C - 15x15"
+tntC15x15 = puzzle (toPuzzle 
+  [
+    "  t   t    t   ",
+    "t         t  tt",
+    "  t t  t  t t  ",
+    "      t     t t",
+    "               ",
+    "   t     t   t ",
+    " t             ",
+    " t    tt t     ",
+    "  t            ",
+    "       t      t",
+    "t       t t    ",
+    "  t      t    t",
+    "   tt t      t ",
+    " t t     t  tt ",
+    "      t t      "
+  ])
+  [6, 1, 4, 1, 3, 2, 3, 2, 4, 0, 5, 1, 4, 2, 5]
+  [4, 2, 3, 3, 1, 4, 1, 6, 0, 3, 3, 3, 2, 3, 5]
 
 
-
--- easy = 
---       println(TentsAndTreesPuzzle(
---         Array(
---           Array('t', ' ', ' '),
---           Array(' ', ' ', ' '),
---           Array(' ', ' ', 't')
---         ),
---         Array(0, 2, 0),
---         Array(1, 0, 1)
---       ).solve)
---     }
-
---     it("8x8 easy") {
---       println(TentsAndTreesPuzzle(
---         Array(
---           " t      ",
---           "   t  t ",
---           "   t   t",
---           "        ",
---           "    t t ",
---           "t  t   t",
---           "      t ",
---           "t       "
---         ).map(_.toCharArray),
---         Array(1, 3, 0, 1, 3, 1, 2, 1),
---         Array(1, 2, 1, 2, 0, 3, 0, 3)
---       ).solve)
---     }
-
---     it("20x20 hard") {
---       println(TentsAndTreesPuzzle(
---         Array(
---           " t   tt    tt   t   ",
---           "tt            t    t",
---           "     t  t         t ",
---           "   t    t tt       t",
---           " t    t       t  t  ",
---           "  t       t t t     ",
---           "          t   tt   t",
---           " t    t         t   ",
---           " t  t       t      t",
---           "    t  t  tt  t     ",
---           "             tt t   ",
---           "      tt          t ",
---           " t  t    t          ",
---           " t  t               ",
---           " t   t     t  t t   ",
---           " t     t t          ",
---           "         t  tt   t t",
---           " tt    t            ",
---           "     tt   t t t t  t",
---           "  t     t      t t  "
---         ).map(_.toCharArray),
---         Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8),
---         Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
---       ).solve)
---     }
-
---     it("TentsAndTrees C - 13x13") {
---       println(TentsAndTreesPuzzle(
---         Array(
---           "t          t ",
---           "t t t    tt  ",
---           "      t     t",
---           "         t t ",
---           "    t   t  t ",
---           " t t      t  ",
---           "        t  t ",
---           " t  t t     t",
---           "t            ",
---           "    t  tt    ",
---           "     t       ",
---           "    t  t t  t",
---           " t   t    t  "
---         ).map(_.toCharArray),
---         Array(2, 4, 2, 4, 2, 2, 4, 2, 2, 3, 2, 2, 4),
---         Array(2, 4, 0, 4, 2, 3, 2, 2, 4, 2, 4, 1, 5)
---       ).solve)
---     }
-
---     it("TentsAndTrees C - 13x13 - 2") {
---       println(TentsAndTreesPuzzle(
---         Array(
---           "             ",
---           "  t    t t  t",
---           "t     tt     ",
---           "   t t     t ",
---           "         t   ",
---           "t    t    t  ",
---           "  t       t  ",
---           "      t     t",
---           "t tt      t  ",
---           "      t    t ",
---           " t  t t  t   ",
---           "      t   tt ",
---           "  t t t      "
---         ).map(_.toCharArray),
---         Array(1, 5, 0, 5, 1, 3, 2, 3, 2, 3, 3, 2, 4),
---         Array(3, 2, 3, 3, 3, 2, 2, 3, 1, 3, 3, 2, 4)
---       ).solve)
---     }
-
---     it("TentsAndTrees C - 13x13 - 3") {
---       println(TentsAndTreesPuzzle(
---         Array(
---           " t  t        ",
---           "     t t    t",
---           "      t  t t ",
---           " t         t ",
---           " t  t  tt    ",
---           "    t     t t",
---           " t  tt       ",
---           "t     t     t",
---           "  tt  t  t   ",
---           "             ",
---           "        t   t",
---           "   t    t t  ",
---           " t   t       "
---         ).map(_.toCharArray),
---         Array(4, 2, 1, 5, 2, 4, 2, 4, 1, 3, 1, 2, 3),
---         Array(3, 2, 2, 4, 2, 2, 3, 3, 2, 4, 1, 1, 5)
---       ).solve)
---     }
-
---     it("TentsAndTrees C - 15x15") {
---       println(TentsAndTreesPuzzle(
---         Array(
---           "  t   t    t   ",
---           "t         t  tt",
---           "  t t  t  t t  ",
---           "      t     t t",
---           "               ",
---           "   t     t   t ",
---           " t             ",
---           " t    tt t     ",
---           "  t            ",
---           "       t      t",
---           "t       t t    ",
---           "  t      t    t",
---           "   tt t      t ",
---           " t t     t  tt ",
---           "      t t      "
---         ).map(_.toCharArray),
---         Array(6, 1, 4, 1, 3, 2, 3, 2, 4, 0, 5, 1, 4, 2, 5),
---         Array(4, 2, 3, 3, 1, 4, 1, 6, 0, 3, 3, 3, 2, 3, 5)
---       ).solve)
---     }
-
---   }
--- }
+tests = undefined
